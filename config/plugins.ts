1d62c7b158c2663832ad3b2135a3b2d2bc8d7524
Hitanshu Gupta <hitanshu.gupta@unisys.com>
2025-09-13 21:34:39 +0530

    update config/plugins.ts for s3 provider

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,17 @@
-export default () => ({});
+// config/env/production/plugins.ts
+
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET_NAME'),
+        },
+      },
+    },
+  },
+});
