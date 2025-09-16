47aac3e79b19cc1ae45dba211b6002b161f227d8
Hitanshu Gupta <hitanshu.gupta@unisys.com>
2025-09-16 13:17:50 +0530

    Updated variables for DB and S3 .ts file

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,3 +1,21 @@
+// // config/env/production/plugins.ts
+
+// export default ({ env }) => ({
+//   upload: {
+//     config: {
+//       provider: 'aws-s3',
+//       providerOptions: {
+//         accessKeyId: env('AWS_ACCESS_KEY_ID'),
+//         secretAccessKey: env('AWS_ACCESS_SECRET'),
+//         region: env('AWS_REGION'),
+//         params: {
+//           Bucket: env('AWS_BUCKET_NAME'),
+//         },
+//       },
+//     },
+//   },
+// });
+
 // config/env/production/plugins.ts
 
 export default ({ env }) => ({
@@ -7,9 +25,12 @@ export default ({ env }) => ({
       providerOptions: {
         accessKeyId: env('AWS_ACCESS_KEY_ID'),
         secretAccessKey: env('AWS_ACCESS_SECRET'),
-        region: env('AWS_REGION'),
-        params: {
-          Bucket: env('AWS_BUCKET_NAME'),
+        s3Options: {
+          region: env('AWS_REGION'),
+          params: {
+            ACL: 'public-read', // This is recommended for most media libraries
+            Bucket: env('AWS_BUCKET_NAME'),
+          },
         },
       },
     },
